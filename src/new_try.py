ded045f4d02628159cf21f0b5541b5c7c82ec959
Tyler Paxman <tylerpaxman@gmail.com>
2020-12-22 12:54:39 -0700

    got the waiting part to work I think

--- src/new_try.py
+++ src/new_try.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 import re
 import pandas as pd
 import sys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 VOLUMES_URL = 'https://scriptures.byu.edu/#::fNYNY7267e401'
 
@@ -25,32 +29,50 @@ def get_verse_url(book_num, chapter_num, verse_num):
     return verse_url
 
 
-# open Chrome webdriver (the latest driver for Chrome 87 is sitting in the project folder)
+# open Chrome to the start page (the latest driver for Chrome 87 is sitting in the project folder)
+CITATION_ID_NAMES = ['citationindex', 'citationindex2']
 browser = webdriver.Chrome()
-
-# configure browser to wait for stuff to load
 browser.implicitly_wait(20)
+browser.get(VOLUMES_URL)
+browser.refresh()
+citation_id_elems = [browser.find_element_by_id(x).get_attribute('innerHTML') for x in CITATION_ID_NAMES]
+citation_id_soups = [BeautifulSoup(x, features="lxml") for x in citation_id_elems]
+links_list = [x for x in citation_id_soups if x.find_all('a')]
+while not links_list:
+    print('trying')
+    citation_id_elems = [browser.find_element_by_id(x).get_attribute('innerHTML') for x in CITATION_ID_NAMES]
+    citation_id_soups = [BeautifulSoup(x, features="lxml") for x in citation_id_elems]
+    links_list = [x for x in citation_id_soups if x.find_all('a')]
+
+# def get_links(javascript_command):
+#
+#     citation_data = soup.find(id=citation_id_str)
+#     for x in citation_data.find_all('a'):
+#         print(x.getText())
+
+    # # to get the linke
+    # a = [x for x in BeautifulSoup(browser.page_source).find(id='citationindex2').find_all('a') if
+    #      ('getFilter' in x.get('onclick')) and (not x.find('span'))]
+    #
+    # # to get the refcounters on the last step
+    # a = [x for x in BeautifulSoup(browser.page_source).find(id='citationindex2').find_all(class_='refcounter')]
+    #
+    # refcounters = [x for x in citation_data.find_all(class_='refcounter') if x.has_attr('onclick')]
+
 
 # Locate the scripture pane on the left side of the page
-verse_url = get_verse_url(101, 11, 4)
-browser.get(verse_url)
-page_source = browser.page_source
-soup = BeautifulSoup(page_source, features="lxml")
 
 # It seems to change between whether 'citationindex' or 'citationindex2' has the data
 # so this chooses the non-empty one
-citation_id_str = [x for x in ('citationindex', 'citationindex2') if soup.find(id=x).find_all(class_='refcounter')][0]
-citation_data = soup.find(id=citation_id_str)
 
-voltitle = citation_data.find(class_='volumetitle').getText()
-
+# voltitle = citation_data.find(class_='volumetitle').getText()
 
 # this is necessary because the center column also has 'refcounter' tags that are not wanted, so we keep only
 # those with the 'onclick' attribute.
-data = [(x.get('onclick'),  # contains IDs for the talk and the reference
-         x.find(class_='reference').getText(),  # contains date and author
-         x.find(class_='talktitle').getText())  # contains the title
-        for x in citation_data.find_all(class_='refcounter') if x.has_attr('onclick')]
+# data = [(x.get('onclick'),  # contains IDs for the talk and the reference
+#          x.find(class_='reference').getText(),  # contains date and author
+#          x.find(class_='talktitle').getText())  # contains the title
+#         for x in citation_data.find_all(class_='refcounter') if x.has_attr('onclick')]
 
 # IDEAS:
 
