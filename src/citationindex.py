fc6f9cd0f0bd6b8e71ff7d9f2f952771a6c206b9
Tyler Paxman <tylerpaxman@gmail.com>
2020-12-27 10:54:55 -0700

    alter get_soup_list function to take page_source

--- src/citationindex.py
+++ src/citationindex.py
@@ -11,17 +11,17 @@ def get_citationindex(browser):
     # This runs until the page has actually loaded and the links are found inside the proper citationindex element
     soup_list = []
     while not soup_list:
-        soup_list = get_soup_list(browser)
+        page_source = browser.page_source
+        soup_list = get_soup_list(page_source)
 
     # the BeautifulSoup object of the citation index element is returned (i.e. for 'citationindex' or 'citationindex2')
     citationindex_soup = soup_list[0]
     return citationindex_soup
 
 
-def get_soup_list(browser):
+def get_soup_list(page_source):
     """Gets the HTML associated with both citation index elements and attempts to find links inside"""
     citation_id_names = ['citationindex', 'citationindex2']
-    page_source = browser.page_source
     citation_id_soups = [BeautifulSoup(page_source, features='lxml').find(id=x) for x in citation_id_names]
     soup_list = [soup for soup in citation_id_soups if soup.find_all('a')]
     return soup_list
